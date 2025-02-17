01055bff5c029b6b354933a3a99a2e8451b2b400
Mohan P <pmk.mohan@gmail.com>
2025-02-17 07:42:20 +0530

    Add files via upload

---
+++ Learning.py
@@ -0,0 +1,8 @@
+a = {1,2,3,5,7}
+b = {5,7,9,10,11}
+c = a.union(b)
+c = a.intersection(b)
+c = a.difference(b)
+D = b.difference(a)
+
+print(c.D)
---
+++ SetMethods.py
@@ -0,0 +1,19 @@
+A = {1,2,3,5,7}
+B ={5,7,9,10,11}
+C = A.union(B)
+D = B.union(A)
+print(C)
+print(D)
+E = A.intersection(B)
+print(E)
+F = B.intersection(A)
+print(F)
+I = A.difference(B)
+print(I)
+J = B.difference(A)
+print(J)
+k = A.symmetric_difference(B)
+print(k)
+J = B.symmetric_difference(A)
+print(J)
+
